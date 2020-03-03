09559fcbc4949956361b27e7c43691f23ceac71d
Kaleb Thornton <krt237@msstate.edu>
2020-03-02 23:28:42 -0600

    Added UnityProject
    - Added Player prefab plus idle, running animations
    - Added Enemy object (no prefab) plus idle animation
    - Enemy now chases the player

---
+++ UnityProject/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    // init some public vars (editable in Unity)
+    public float speed;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // on startup, set target to the player
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Update called on a fixed time interval
+    private void FixedUpdate()
+    {
+        // chase after the player
+        if (Vector2.Distance(transform.position, target.position) > 2f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // init some public vars (editabl in Unity)
+    public CharacterController2D controller;
+    public Animator animator;
+    public float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    // init some private vars
+    bool jump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from the user
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (horizontalMove != 0)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+        
+
+        // Check if the user is trying to jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJump", true);
+        }
+        else
+        {
+            animator.SetBool("isJump", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
